b7ef09f49ceb50e4b26a1b27c04d3298170e287a
Rasande <andreas@rasande.se>
2020-02-12 17:35:29 +0100

    Remove pingback

--- header.php
+++ header.php
@@ -14,7 +14,6 @@ defined( 'ABSPATH' ) || exit;
         <head>
             <meta charset="<?php bloginfo( 'charset' ); ?>">
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-            <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
             <link rel="profile" href="http://gmpg.org/xfn/11">
             <?php wp_head(); ?>
         </head>
