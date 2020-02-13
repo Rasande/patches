f4bde76ef3a64ea7141625b54cd2b8478eb96284
Rasande <andreas@rasande.se>
2020-02-13 17:22:25 +0100

    Create navbar-toggle.php

---
+++ parts/navbar-toggle.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * Navbar toggle part.
+ * 
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+?>
+
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
+    <span class="navbar-toggler-icon"></span>
+</button>
