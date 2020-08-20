3eba91c17eb090595e3df3470ec7977edfdeb97d
Rasande <andreas@rasande.se>
2020-08-20 11:37:06 +0200

    Create page.php

---
+++ page.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Template file for pages
+ *
+ */
+
+// Exit if accessed directly.
+defined('ABSPATH') || exit;
+
+get_header(); ?>
+
+<div class="site-content">
+    <main class="main">
+
+        <?php if (have_posts()) :
+            while (have_posts()) : the_post(); ?>
+
+                <!-- Entry -->
+                <article class="entry">
+                    <div class="container">
+
+                        <!-- Entry header -->
+                        <?php get_template_part('parts/entry', 'header'); ?>
+
+                        <!-- Entry content -->
+                        <div class="entry-content">
+                            <?php the_content(); ?>
+                        </div>
+
+                    </div>
+                </article>
+
+            <?php endwhile; ?>
+        <?php endif; ?>
+
+    </main>
+</div>
+
+<?php get_footer(); ?>
