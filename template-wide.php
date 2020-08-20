23d65702c303ce7c5241cdd8e35cf55281ebf51e
Rasande <andreas@rasande.se>
2020-08-20 11:35:55 +0200

    Create template-wide.php

---
+++ template-wide.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ *  Template name: Wide
+ * 
+ *  A template with a wide content container
+ *
+ */
+
+// Exit if accessed directly.
+defined('ABSPATH') || exit;
+
+get_header(); ?>
+
+<!-- Site content -->
+<div class="site-content">
+    <main class="main">
+
+        <?php if (have_posts()) :
+            while (have_posts()) : the_post(); ?>
+
+                <!-- Entry -->
+                <article class="entry">
+
+                    <div class="container">
+                        <!-- Entry header -->
+                        <?php get_template_part('parts/entry', 'header'); ?>
+                    </div>
+
+                    <div class="container-wide">
+                        <!-- Entry content -->
+                        <div class="entry-content">
+                            <?php the_content(); ?>
+                        </div>
+
+                    </div>
+                </article>
+
+            <?php endwhile; ?>
+        <?php endif; ?>
+
+    </main>
+</div>
+
+<?php get_footer(); ?>
