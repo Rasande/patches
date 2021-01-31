7825eaa6ac49308fb9ababb918292eb1ea9b9619
Rasande <andreas@rasande.se>
2021-01-31 20:05:16 +0100

    Update fluid-type mixin

--- templates/template-frontpage.php
+++ templates/template-frontpage.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * 
  * Template name: Frontpage
@@ -13,7 +14,19 @@ get_header(); ?>
 <!-- Site content -->
 <main class="site-main">
 
+    <?php if (have_posts()) :
+        while (have_posts()) : the_post(); ?>
 
+            <!-- Page header -->
+            <?php get_template_part('template-parts/hero'); ?>
+
+            <!-- Page content -->
+            <div class="block-content">
+                <?php the_content(); ?>
+            </div>
+
+        <?php endwhile; ?>
+    <?php endif; ?>
 
 </main>
 
