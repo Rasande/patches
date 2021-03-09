8521978156cb50d452f26c772f53f190837d6ce2
Rasande <andreas@rasande.se>
2021-03-09 13:43:50 +0100

    Add time difference function

--- inc/theme-functions.php
+++ inc/theme-functions.php
@@ -9,13 +9,24 @@ defined('ABSPATH') || exit;
 
 // Read time for post
 if (!function_exists('rasande_read_time')) {
-    function rasande_read_time($postID)
+    function rasande_read_time()
     {
+        $postID = get_the_ID();
         $content = get_post_field('post_content', $postID);
         $word_count = str_word_count(strip_tags($content));
         $readingtime = ceil($word_count / 250);
         $timer = " min";
         $totalreadingtime = $readingtime . $timer;
+
         return $totalreadingtime;
+    }
+}
+
+if (!function_exists('rasande_time_diff')) {
+    function rasande_time_diff() {
+        $posted = get_post_time();
+        $date = $posted >= strtotime('-2 weeks') ? human_time_diff($posted) . ' ' . __('ago', 'rasande') :  get_the_date(); ;
+
+        return $date;
     }
 }
