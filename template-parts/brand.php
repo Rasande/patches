090d4d4753f54ccf1599060d77fae7788201c391
Rasande <andreas@rasande.se>
2021-03-23 14:50:15 +0100

    Update brand.php

--- template-parts/brand.php
+++ template-parts/brand.php
@@ -1,16 +1,16 @@
-<?php
+    <?php
 
-/**
- * Brand
- * 
- */
+    /**
+     * Brand
+     * 
+     */
 
-defined('ABSPATH') || exit; ?>
+    defined('ABSPATH') || exit; ?>
 
-<?php if (!has_custom_logo()) : ?>
+    <?php if (!has_custom_logo()) : ?>
 
-    <a class="brand" rel="home" href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" itemprop="url"><?php bloginfo('name'); ?></a>
+        <a class="brand" rel="home" href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" itemprop="url"><?php bloginfo('name'); ?></a>
 
-<?php else :
-    the_custom_logo();
-endif; ?>
+    <?php else :
+        the_custom_logo();
+    endif; ?>
